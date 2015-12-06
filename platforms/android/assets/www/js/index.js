b1cd0821e9b4f1844ded24d4474d872370c404cf
weekheongquape <wee.kheong@quape.com>
2015-12-06 19:56:57 +0800

    loges fug

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -51,7 +51,7 @@ var app = {
         var pushNotification = window.plugins.pushNotification;
         // TODO: Enter your own GCM Sender ID in the register call for Android
         if (device.platform == 'android' || device.platform == 'Android') {
-            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"554205989074","ecb":"app.onNotificationGCM"});
+            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"930119857297","ecb":"app.onNotificationGCM"});
         }
         else {
             pushNotification.register(this.tokenHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});

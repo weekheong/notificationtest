039cdf33005ebf0597e6e37fb4e87c7e8171c78b
weekheongquape <wee.kheong@quape.com>
2015-12-06 20:40:11 +0800

    TEST

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -51,6 +51,7 @@ var app = {
         var pushNotification = window.plugins.pushNotification;
         // TODO: Enter your own GCM Sender ID in the register call for Android
         if (device.platform == 'android' || device.platform == 'Android') {
+			alert("Android");
             pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"930119857297","ecb":"app.onNotificationGCM"});
         }
         else {
